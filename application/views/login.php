d461a85b34e9e7e4cad45ff51fb801dd15c5f631
Christian Dave Aplasca <christiandaveaplasca@gmail.com>
2019-08-05 11:20:34 +0800

    Login Form Bootstrap Complete

--- application/views/login.php
+++ application/views/login.php
@@ -1,53 +1,88 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title><?php echo $title; ?></title>
-
-	<!-- bootstrap css -->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
-	<!-- boostrap theme -->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bootstrap/css/bootstrap-theme.min.css') ?>">
-
-	<!-- custom css -->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('custom/css/custom.css') ?>">	
-
-	<!-- jquery -->
-	<script type="text/javascript" src="<?php echo base_url('assets/jquery/jquery.min.js') ?>"></script>
-	<!-- boostrap js -->
-	<script type="text/javascript" src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js') ?>"></script>
-
+	<title>Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="assets/admin/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/admin/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/admin/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/admin/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/admin/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="assets/admin/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/admin/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/admin/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="assets/admin/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/admin/css/util.css">
+	<link rel="stylesheet" type="text/css" href="assets/admin/css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('assets/admin/images/fcu.jpg');">
+			<div class="wrap-login100">
+			<form method="post" action="<?php echo base_url('users/login'); ?>" id="loginForm">
+				<div class="avatar">
+				<center><img src="assets/admin/images/download.jfif" style="border-radius: 80%;" alt="Avatar"></center>
+			</div>
+			
+				
 
+					<span class="login100-form-title p-b-20 p-t-20">
+						Student Management System
+					</span>
 
-<div class="col-md-6 col-md-offset-3 vertical-off-4">
-	<div class="panel panel-default login-form">
-	  	<div class="panel-body">
-	  		<form method="post" action="<?php echo base_url('users/login') ?>" id="loginForm">
-		    	<fieldset>
-		    		<legend>
-		    			Login
-		    		</legend>
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					</div>
 
-		    		<div id="message"></div>
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xf191;"></span>
+					</div>
 
-					<div class="form-group">
-						<label for="username">Username</label> 
-				    	<input type="text" class="form-control" id="username" name="username" placeholder="Username" autofocus>
-				  	</div>
-				  	<div class="form-group">
-				    	<label for="password">Password</label>
-				    	<input type="password" class="form-control" id="password" name="password" placeholder="Password">
-				  	</div>					  						 
-				  	
-				  	<button type="submit" class="col-md-12 btn btn-primary login-button">Submit</button>					
-		    	</fieldset>
-		    </form>
-	  	</div>
+					<div class="container-login100-form-btn">
+						<button type="submit" class="login100-form-btn">
+							Login
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
 	</div>
-</div>
+	
 
-<script type="text/javascript" src="<?php echo base_url('custom/js/login.js') ?>"></script>
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="assets/admin/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/admin/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/admin/vendor/bootstrap/js/popper.js"></script>
+	<script src="assets/adminv/endor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/admin/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/admin/vendor/daterangepicker/moment.min.js"></script>
+	<script src="assets/admin/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/admin/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="assets/admin/js/main.js"></script>
+	<script type="text/javascript" src="<?php echo base_url('custom/js/login.js') ?>"></script>
 
 </body>
 </html>
